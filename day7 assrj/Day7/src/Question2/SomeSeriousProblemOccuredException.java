269e826fceebc4cfba19182527ab8934f1c454d9
falgun bokde <falgunbokade@gmail.com>
2022-11-25 16:42:31 +0530

    day 7 ass submited

---
+++ day7 assrj/Day7/src/Question2/SomeSeriousProblemOccuredException.java
@@ -0,0 +1,15 @@
+package Question2;
+
+import java.util.Scanner;
+
+public class SomeSeriousProblemOccuredException extends Exception{
+	
+//	
+//	}
+//	public class SimpleProgram{
+	public static void main(String...args){
+	Scanner scanner = new Scanner(System.in);
+	//some statements throwing SomeSeriousProblemOccuredException;
+	scanner.close();
+	}
+	}
